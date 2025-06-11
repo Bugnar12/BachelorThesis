bd08a2ed5bdb10c6a2e74c3bd9579fd795e78a7c
Bugnar12 <eduardbugnaru@gmail.com>
2025-06-11 23:54:48 +0300

    small fix which includes jwt

--- Backend/Flask_Backend/service/email_service.py
+++ Backend/Flask_Backend/service/email_service.py
@@ -41,7 +41,6 @@ class EmailService:
         logger.info("Domain in virusTotal: {}".format(domain))
         vt_dns_info = self.__vt_service.get_vt_dns_info(domain)
         vt_report = self.__vt_service.get_vt_dns_report_results(vt_dns_info)
-        logger.info("vt_dns_info logged: {}".format(vt_dns_info))
         logger.info("vt_report logged: {}".format(vt_report))
 
         return {
